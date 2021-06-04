0e2af8fd5a654ec779a6feb9f5607f1c684971b3
sumaanthg <79955349+sumaanthg@users.noreply.github.com>
2021-06-04 18:23:53 +0530

    Testing

---
+++ ContactForm.php
@@ -0,0 +1,12 @@
+<?php
+$data=json_decode(file_get_contents("php://input"));
+
+$name=mysql_real_escape_string($data->name);
+$email=mysql_real_escape_string($data->email);
+$subject=mysql_real_escape_string($data->subject);
+$message=mysql_real_escape_string($data->message);
+mysql_connect("localhost","root","");
+mysql_select_db("contactform");
+mysql_query("insert into data(`name`,`email`,`subject`,`message`) values('".$name."','".$email."','".$subject"','".$message"')");
+
+?>
